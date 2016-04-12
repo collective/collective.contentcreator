569bec541bf552e3a25c6e396d0d4b508f78461d
Johannes Raggam <thetetet@gmail.com>
2016-04-12 16:34:21 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""Installer for the collective.contentcreator package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = '\n\n'.join([
+    open('README.rst').read(),
+    open('CONTRIBUTORS.rst').read(),
+    open('CHANGES.rst').read(),
+])
+
+
+setup(
+    name='collective.contentcreator',
+    version='1.0a1',
+    description="Create content structures from JSON configurations",
+    long_description=long_description,
+    # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+    keywords='Python Plone',
+    author='Johannes Raggam',
+    author_email='thetetet@gmail.com',
+    url='https://pypi.python.org/pypi/collective.contentcreator',
+    license='GPL version 2',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'plone.api',
+        'Products.GenericSetup>=1.8.2',
+        'setuptools',
+        'z3c.jbot',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'plone.app.contenttypes',
+            'plone.app.robotframework[debug]',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
